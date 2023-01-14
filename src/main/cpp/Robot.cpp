c721fb5e83bdcfd652f118310f71b52c58a812dc
ch1kim0n1 <chikimoni61@gmail.com>
2023-01-14 17:48:38 -0600

    Auto + Intake

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -6,25 +6,34 @@
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/motorcontrol/PWMSparkMax.h>
 #include <frc/Timer.h>
-#include <frc/TimedRobot.h>
+#include <frc/Notifier.h>
 #include <frc/motorcontrol/MotorControllerGroup.h>
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <rev/CANSparkMax.h>
-//hi
+#include <frc/smartdashboard/SendableChooser.h>
+#define WPILIB_NO_UNITS
+
+  //auto options
+   frc::SendableChooser<std::string> m_chooser;
+   const std::string kAutoDefault = "NONE";
+   const std::string kAutoFowrard3 = "Forward_3sec";
+
 /**
  * This is a demo program showing the use of the DifferentialDrive class.
  * Runs the motors with tank steering.
  */
 class Robot : public frc::TimedRobot {
-  //acceleration feature
-  //double Acceleration = 0.3;
-  double currentSpeed = 0;
 
   //creating 4 motors as it is in the robot
   rev::CANSparkMax m_leftMotor1{1, rev::CANSparkMax::MotorType::kBrushed};
   rev::CANSparkMax m_leftMotor2{2, rev::CANSparkMax::MotorType::kBrushed};
   rev::CANSparkMax m_rightMotor1{3, rev::CANSparkMax::MotorType::kBrushed};
   rev::CANSparkMax m_rightMotor2{4, rev::CANSparkMax::MotorType::kBrushed};
+
+  //2 motors for the intake
+  rev::CANSparkMax m_leftIntake{10, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_rightIntake{5, rev::CANSparkMax::MotorType::kBrushless};
+
   //create a MotorControllerGroup to combine all left and right motors
   frc::MotorControllerGroup m_leftMotors{m_leftMotor1, m_leftMotor2};
   frc::MotorControllerGroup m_rightMotors{m_rightMotor1, m_rightMotor2};
@@ -33,33 +42,79 @@ class Robot : public frc::TimedRobot {
   frc::DifferentialDrive m_robotDrive{m_leftMotors, m_rightMotors};
   frc::XboxController controller{0}; 
 
+  //AUTO
+  frc::Timer timer;
+  bool move_forward;
+  //frc::Notifier notifier();
+
  public:
   void RobotInit() override {
+    //make motors reset to 0
     m_leftMotor1.RestoreFactoryDefaults();
     m_leftMotor2.RestoreFactoryDefaults();
     m_rightMotor1.RestoreFactoryDefaults();
     m_rightMotor2.RestoreFactoryDefaults();
+
+    //m_rightIntake.RestoreFactoryDefaults();
     //right motor must be inverted for it to go forward
     m_rightMotors.SetInverted(true);
+    
+    //give auto options (Now only 2)
+    m_chooser.SetDefaultOption(kAutoDefault, kAutoDefault);
+    m_chooser.AddOption(kAutoFowrard3, kAutoFowrard3);
+    frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+    //give a 0 as initial number for intake speed
+    frc::SmartDashboard::PutNumber("Intake Speed", 0); //check if it will give me an option to change a number
   }
 
   void TeleopPeriodic() override {
-    //get the speed and roataion from the controller
-    //Left Joystic - Y Axis for speed
+    //spped + rotatation
     double speed = controller.GetLeftY();
-    //Right Joystic - X Axis for rotation
     double rotation = controller.GetRightX();
-
-    //creating current speed by * speed and acceleratiton
-    //currentSpeed += speed * Acceleration;
-
-    //Use ArcadeDrive to eliminate usage of multiple if/else statements
-    //also speed * kMaxSpeed to create acceleration for the robot
+    //Arcade drive method
     m_robotDrive.ArcadeDrive(speed, rotation);
 
-    //code to check the speed of the robot without acceleration
-    //m_robotDrive.ArcadeDrive(speed, rotation);
+    //get number from smartDashboard (if not picked, it will be 0.5)
+    int intakenum = frc::SmartDashboard::GetNumber("Intake Speed", 0.5);
+    //initial speed of the intake
+    int intakeSpeed = 0;
+
+    //check for left and right trigger
+    if (controller.GetLeftTriggerAxis() > 0) {
+        intakeSpeed = intakenum;
+    }
+    else if (controller.GetRightTriggerAxis() > 0) {
+        intakeSpeed = -intakenum;
+    }
+    //set inverted motor speed to 2nd motor
+    m_leftIntake.Set(intakeSpeed);
+    m_rightIntake.Set(-intakeSpeed);
   }
+
+    //*************************************************AUTONOMUS PART*************************************************************
+  void autonomousInit() {
+    move_forward = false;
+    timer.Reset();
+    timer.Start();
+  }
+  
+
+  void autonomousPeriodic() {
+    std::string selectedOption = frc::SmartDashboard::GetString("Auto Modes", "Forward_3sec");
+    if (selectedOption == "Forward_3sec") {
+        move_forward = true;
+        if (units::unit_cast<double>(timer.Get()) < 3.0) {
+            //Drive forward for 3 seconds at 25% of the motor capacity
+            m_robotDrive.ArcadeDrive(0.25, 0);
+        } else {
+            m_robotDrive.ArcadeDrive(0, 0);
+        }
+    } else {
+        move_forward = false;
+    }
+  }
+  //**************************************************************************************************************************
 };
 
 #ifndef RUNNING_FRC_TESTS
