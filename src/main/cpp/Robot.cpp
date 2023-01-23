632cc325bebe1002d0812dbe8b8201048fcce448
ch1kim0n1 <chikimoni61@gmail.com>
2023-01-23 16:51:58 -0600

    update

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -28,6 +28,63 @@
  */
 class Robot : public frc::TimedRobot {
 
+  //insersted from EVERYBOT EXAMPLE CODE-----------------------------
+  //VictorSPX driveLeftVictor = new VictorSPX(3);
+  //VictorSPX driveRightVictor = new VictorSPX(4);
+
+  /**
+   * How many amps the arm motor can use.
+   */
+  static const int ARM_CURRENT_LIMIT_A = 20;
+
+  /**
+   * Percent output to run the arm up/down at
+   */
+  static const double ARM_OUTPUT_POWER = 0.4;
+
+  /**
+   * How many amps the intake can use while picking up
+   */
+  static const int INTAKE_CURRENT_LIMIT_A = 25;
+
+  /**
+   * How many amps the intake can use while holding
+   */
+  static const int INTAKE_HOLD_CURRENT_LIMIT_A = 5;
+
+  /**
+   * Percent output for intaking
+   */
+  static const double INTAKE_OUTPUT_POWER = 1.0;
+
+  /**
+   * Percent output for holding
+   */
+  static const double INTAKE_HOLD_POWER = 0.07;
+
+  /**
+   * Time to extend or retract arm in auto
+   */
+  static const double ARM_EXTEND_TIME_S = 2.0;
+
+  /**
+   * Time to throw game piece in auto
+   */
+  static const double AUTO_THROW_TIME_S = 0.375;
+
+  /**
+   * Time to drive back in auto
+   */
+  static const double AUTO_DRIVE_TIME = 6.0;
+
+  /**
+   * Speed to drive backwards in auto
+   */
+  static const double AUTO_DRIVE_SPEED = -0.25;
+  //--------------------------------------------------------------------
+
+
+
   //creating 4 motors as it is in the robot
   rev::CANSparkMax m_leftMotor1{1, rev::CANSparkMax::MotorType::kBrushed};
   rev::CANSparkMax m_leftMotor2{2, rev::CANSparkMax::MotorType::kBrushed};
@@ -35,7 +92,7 @@ class Robot : public frc::TimedRobot {
   rev::CANSparkMax m_rightMotor2{4, rev::CANSparkMax::MotorType::kBrushed};
 
   //2 motors for the intake
-  rev::CANSparkMax m_leftIntake{12, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_leftIntake{12, rev::CANSparkMax::MotorType::kBrushless}; //prob need to change IDs
   rev::CANSparkMax m_rightIntake{5, rev::CANSparkMax::MotorType::kBrushless};
 
   //create a MotorControllerGroup to combine all left and right motors
@@ -64,7 +121,7 @@ class Robot : public frc::TimedRobot {
     //right motor must be inverted for it to go forward
     m_rightMotors.SetInverted(true);
     
-    //give auto options (Now only 2)
+    //list of Auto Options
     m_chooser.SetDefaultOption("NONE", kAutoDefault);
     m_chooser.AddOption("Red_Mid", kAutoFowrard1);
     m_chooser.AddOption("Red_Right", kAutoFowrard2);
@@ -80,11 +137,9 @@ class Robot : public frc::TimedRobot {
     //spped + rotatation
     double speed = controller.GetLeftY();
     double rotation = controller.GetRightX();
-
-    //Arcade drive method
     m_robotDrive.ArcadeDrive(speed, rotation);
 
-    //initial speed of the intake
+    //initial speed of the intake will be 40%
     double intakeSpeed = frc::SmartDashboard::GetNumber("Intake Speed", 0.4);
 
     //check for left and right trigger
@@ -99,7 +154,7 @@ class Robot : public frc::TimedRobot {
         m_rightIntake.Set(-intakeSpeed);
     }
     else {
-        //otherwise set them to 0
+        //if none, set to 0
         m_leftIntake.Set(0);
         m_rightIntake.Set(0);
     }
@@ -110,43 +165,35 @@ class Robot : public frc::TimedRobot {
   void AutonomousInit() override {
     m_timer.Reset();
     m_timer.Start();
-    //m_waittimer.Reset();
-    //m_waittimer.Start();
   }
 
   void AutonomousPeriodic() override {
-    bool wait = true;
+    //initial values for the auto
     int x = frc::SmartDashboard::GetNumber("Delay (Sec)", 0);
     double intakeSpeed = frc::SmartDashboard::GetNumber("Intake Speed", 0.8);
     std::string selectedOption = m_chooser.GetSelected();
-    //--------------------------DELAY SYSTEM--------------------------------------
-    //  if(!wait){
-        units::unit_t<units::time::second, double, units::linear_scale> secondsX(x);
-    //    //std::this_thread::sleep_for(std::chrono::seconds(seconds));
-    //     if(m_timer.Get() < secondsX) {
-    //      m_robotDrive.ArcadeDrive(0.0, 0.0, false);
-    //    }
-    //    else if(m_timer.Get() == secondsX){
-    //       m_robotDrive.ArcadeDrive(0.0, 0.0, false);
-    //      wait = false;
-    //    }
-    //    else{
-    //     m_robotDrive.ArcadeDrive(0.0, 0.0, false);
-    //    }
-    //  }
+    //convertion of double into seconds
+    units::unit_t<units::time::second, double, units::linear_scale> secondsX(x);
 
-    //---------------------------------------------------------------------------
-    //if(wait == false){
+
     if (selectedOption == "Red_Left") {
       //ROBOT MUST FACE THE DRIVER
+
+      //What this Auto is doing:
+      //1. facing driver it goes a little forward with object and release it
+      //2. turn 90 degrees to the left + forward for a few meters
+      //3. turn left again facing the bridge
+      //4. getting up to the bridge with lower speed 
       if(m_timer.Get() < secondsX){
         m_robotDrive.ArcadeDrive(0.0, 0.0, false);
       }
       else if(m_timer.Get() < 0.2_s + secondsX){ //+0.75s is perfect time to turn 90 degrees
         m_robotDrive.ArcadeDrive(-0.6, 0.0, false); 
+        //INTAKE ON
       }
       else if (m_timer.Get() < 0.85_s + secondsX){
         m_robotDrive.TankDrive(0.7, -0.7, false);
+        //INAKE OFF
       }
       else if (m_timer.Get() < 1.85_s + secondsX){
         m_robotDrive.ArcadeDrive(-0.5, 0, false);
@@ -161,8 +208,16 @@ class Robot : public frc::TimedRobot {
         m_robotDrive.ArcadeDrive(0, 0, false); 
       }
     }
+
+
     else if(selectedOption == "Red_Mid"){
-      //ROBOT MUST FACE THE FIELD
+      //ROBOT MUST FACE THE DRIVER
+
+      //What this Auto is doing:
+      //1. facing driver it goes a little forward with object and release it
+      //2. turn 180 degrees to the left + forward - slow to get through te bridge
+      //3. get the object in the middle of the field
+      //4. may chose to go both to the left or right.
       if(m_timer.Get() < secondsX){
         m_robotDrive.ArcadeDrive(0.0, 0.0, false);
       }
@@ -180,14 +235,12 @@ class Robot : public frc::TimedRobot {
       }
       else if(m_timer.Get() < 3.6_s + secondsX){ 
         m_robotDrive.ArcadeDrive(0, 0, false);
-        //m_robotDrive.ArcadeDrive(-.9, 0, false);
       }
       else if(m_timer.Get() < 4.4_s + secondsX){ 
         m_robotDrive.ArcadeDrive(-.9, 0, false);
       }
       else if(m_timer.Get() < 5.65_s + secondsX){ 
         m_robotDrive.ArcadeDrive(0, 0, false);
-        //m_robotDrive.TankDrive(0.-7, 0.7, false); 
       }
       else if(m_timer.Get() < 6.7_s + secondsX){ 
         m_robotDrive.TankDrive(-0.6, 0.6, false); 
@@ -208,8 +261,16 @@ class Robot : public frc::TimedRobot {
         m_robotDrive.ArcadeDrive(0, 0, false); 
       }
     }
+
+
     else if(selectedOption == "Red_Right"){
       //ROBOT MUST FACE THE DRIVER
+
+      //What this Auto is doing:
+      //1. facing driver it goes a little forward with object and release it
+      //2. turn 180 degrees to the left + forward until obejct is hit
+      //3. capture the object and turn 180 degrees
+      //4. go back and put the object in different place
       if(m_timer.Get() < secondsX){
         m_robotDrive.ArcadeDrive(0.0, 0.0, false);
       }
@@ -243,7 +304,7 @@ class Robot : public frc::TimedRobot {
       else if(m_timer.Get() < 6.1_s + secondsX){
           m_robotDrive.TankDrive(0, 0, false); 
           m_leftIntake.Set(0);
-          m_rightIntake.Set(0); 
+          m_rightIntake.Set(0);
       }
       else if(m_timer.Get() < 7.35_s + secondsX){
           m_robotDrive.ArcadeDrive(-0.75, 0.0, false); 
@@ -261,7 +322,6 @@ class Robot : public frc::TimedRobot {
           m_robotDrive.TankDrive(0, 0.0, false);
       }
     }
-    //}
     
   }
   //**************************************************************************************************************************
